1477291ac6e47c90297b3908045dc705518b4a88
ZaRyAaB0419 <kinghereah@gmail.com>
2024-09-17 12:57:26 +0500

    Add files via upload

---
+++ CDProject.py
@@ -0,0 +1,21 @@
+
+t = float(input("Enter the temperature: "))
+
+if 0 <= t <= 10:
+    print("Wear a sweater")
+
+if 10 <= t <= 20:
+    print("Wear a warm clothe")
+
+if 20 <= t <= 30:
+    print("Wear Summer Clothes☀️")
+
+if 30 <= t <= 45:
+    print("Wear a T-shirt👕")
+
+
+
+
+
+
+
